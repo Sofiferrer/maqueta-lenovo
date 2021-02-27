96572df66da09fe71ca445dfc0fcbe53bf54d15f
Sofia Ferrer <chopotolina@gamil.com>
2021-02-27 15:30:36 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        img: "/img/legionY520.jpeg",
+        name: "",
+        span: "",
+        price: "",
+    },
+    {
+        img: "/img/legionY920.jpeg",
+        name: "",
+        span: "",
+        price: "",
+    },
+    {
+        img: "/img/legionY700.jpeg",
+        name: "",
+        span: "",
+        price: "",
+    },
+    {
+        img: "/img/ideaCentreY900.jpeg",
+        name: "",
+        span: "",
+        price: "",
+    },
+]
